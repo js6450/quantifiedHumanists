a9f905232b729ab9cf1fa44e52186df5400f1778
Jiwon <js6450@nyu.edu>
2019-02-25 04:05:18 -0500

    add code for my data feels

---
+++ assignments/week3/code/public/Views.js
@@ -0,0 +1,100 @@
+class Views {
+    constructor() {
+
+      this.loginHTML = `
+      <main class="login container">
+          <section>
+              <h1>My Data Feelings Log</h1>
+              <p>A generative data visualization of my feelings submitted every day.</p>
+          </section>
+          <section>
+          <h2 class="heading">Log in or signup</h2>
+          <form class="form">
+              <fieldset>
+                <input class="block" type="email" name="email" placeholder="email">
+              </fieldset>
+      
+              <fieldset>
+                <input class="block" type="password" name="password" placeholder="password">
+              </fieldset>
+      
+              <button type="button" id="login" class="">
+                Log in
+              </button>
+      
+              <button type="button" id="signup" class="">
+                Sign up and log in
+              </button>
+            </form>
+          </div>
+          <section>
+      
+      </main>
+      `;
+
+      this.visHTML = `
+      <main>
+          <header>
+              <h1>My Data Feelings <small class="btn" id="admin">⚙︎</small><small id="logout"  class="btn" style="font-size:12px; margin-left:20px; cursor:pointer">logout</small></h1>
+              <p>This is a series of daily visuals generated from my data feelings </p>
+              
+          </header>
+            <!-- all of our sketches will be added here -->
+            <section class="grid-container" id="vis-grid"></section>
+      </main>
+      `
+
+      this.adminHTML = `
+          <main>
+          <header>
+              <h1>Data Feelings Admin Console</h1>
+              <p>Use the various inputs to submit data about your mood</p>
+              <small class="btn" style="text-decoration:underline" id="vis">🔙 back to viz</small>
+          </header>
+          <section class="grid-container">
+          
+            <div class="" id="myControls">
+              <form id="moodForm" onsubmit="submitFeelings()" method="post">
+              <fieldset>
+                <legend>mood</legend>
+                <input type="text" id="mood-input" name="mood" placeholder="e.g. happy">
+              </fieldset>
+              <fieldset>
+                  <legend>Anxiety</legend>
+                  <input type="range" id="anxiety-input" name="anxiety" placeholder="e.g. happy" min="1" max="10">
+                  <span id="anxiety-input-label"></span>
+              </fieldset>
+              <fieldset>
+                  <legend>stress</legend>
+                  <input type="range" id="stress-input" name="stress" placeholder="e.g. happy" min="1" max="10">
+                  <span id="stress-input-label"></span>
+              </fieldset>
+              <fieldset>
+                  <legend>contentment</legend>
+                  <input type="range" id="contentment-input" name="contentment" placeholder="e.g. happy" min="1" max="10">
+                  <span id="contentment-input-label"></span>
+              </fieldset>
+              <fieldset>
+                  <legend>productivity</legend>
+                  <input type="range" id="productivity-input" name="productivity" placeholder="e.g. happy" min="1" max="10">
+                  <span id="productivity-input-label"></span>
+              </fieldset>
+              <fieldset>
+                  <legend>fitness today?</legend>
+                  <div>
+                      <p>Yes 😍</p>
+                      <input type="radio" id="fitness-input-yes" name="fitness" value="yes"> 
+                  </div>
+                  <div>
+                  <p>No 😭</p>
+                  <input type="radio" id="fitness-input-no" name="fitness" value="no" checked> 
+                  </div>
+              </fieldset>
+            </form>
+            <button id="submit-input">Submit! 🚀</button>
+            </div>
+            </section>
+          </main>
+      `
+    }
+  }
---
+++ assignments/week3/code/public/index.js
@@ -0,0 +1,218 @@
+let views;
+let app;
+// NOTE: client will be a variable that interfaces with feathers backend
+
+function setup() {
+  // no canvas needed
+  noCanvas();
+  // noLoop since we dont need the draw loop;
+  noLoop();
+
+  views = new Views();
+  app = new App(views);
+  app.setup()
+}
+
+function draw() {
+  // Add main here
+
+  app.login()
+}
+
+
+class App {
+  // NOTE: Requires p5.js dom library (e.g. see select())
+  constructor(_views) {
+    this.views = _views;
+    this.setup = this.setup.bind(this)
+    this.submitFeelings = this.submitFeelings.bind(this)
+  }
+
+  async showViz() {
+    select('#app').elt.innerHTML = this.views.visHTML;
+
+    let dataFeelings = await client.service('feelings').find({
+      query: {
+        $sort: {
+          createdAt: -1
+        },
+        $limit: false,
+
+      }
+    });
+
+    this.createDataFeelings(dataFeelings)
+
+  }
+
+  createDataFeelings(_dataFeelings) {
+    _dataFeelings.data.forEach((item, idx) => {
+      let newSketch = new MyFeelings(item);
+      let newDiv = createDiv()
+      let divId = `vis-${idx}`
+      newDiv.parent('vis-grid');
+      newDiv.class('grid-item');
+      newDiv.elt.id = divId;
+      newDiv.elt.innerHTML = `<small style="font-size:9px">${item.createdAt}</small><br><small style="font-size:9px">${item.mood}</small>`
+      new p5(newSketch.render, divId);
+    })
+  }au
+
+  async showAdmin() {
+    select("#app").elt.innerHTML = views.adminHTML;
+
+    // select the inputs to track if and when they change
+    const anxietyInput = select("#anxiety-input");
+    const stressInput = select("#stress-input");
+    const contentmentInput = select("#contentment-input");
+    const productivityInput = select("#productivity-input");
+    const submitInput = select("#submit-input");
+
+    // on buttonpress submit feelings!
+    submitInput.mousePressed(this.submitFeelings);
+    // create an array of those inputs to iterate through them
+    // we will set the inital state of the slider labels
+    // we will also make sure to change the slider label value on .changed()
+    const inputs = [anxietyInput, stressInput, contentmentInput, productivityInput];
+    inputs.forEach(i => {
+      // set the initial value
+      let label = select(`#${i.elt.id}-label`)
+      label.elt.innerHTML = i.elt.value
+
+      // if it changes, then update it by adding an event listener
+      i.changed(function (e) {
+        label.elt.innerHTML = i.elt.value;
+      })
+    });
+  }
+
+  showLogin(error) {
+    if (selectAll('.login').length) {
+      select('.heading').elt.insertAdjacentHTML('beforeend', `<p>There was an error: ${error.message}</p>`);
+    } else {
+      select('#app').elt.innerHTML = views.loginHTML;
+    }
+  }
+
+  getCredentials() {
+    const user = {
+      email: document.querySelector('[name="email"]').value,
+      password: document.querySelector('[name="password"]').value
+    };
+
+    return user;
+  };
+
+  async submitFeelings(e) {
+    try {
+      //   e.preventDefault();
+      console.log("submitted!")
+      const myForm = new FormData(document.querySelector("#moodForm"));
+      const payload = {
+        anxiety: myForm.get('anxiety'),
+        contentment: myForm.get('contentment'),
+        fitness: myForm.get('fitness'),
+        mood: myForm.get('mood'),
+        productivity: myForm.get('productivity'),
+        stress: myForm.get('stress')
+      }
+      await client.authenticate();
+      let newData = await feelings.create(payload);
+      await this.showViz();
+
+    } catch (error) {
+      return error;
+    }
+  }
+t
+
+
+  async login(credentials) {
+    try {
+      if (!credentials) {
+        // Try to authenticate using the JWT from localStorage
+        await client.authenticate();
+      } else {
+        // If we get login information, add the strategy we want to use for login
+        const payload = Object.assign({
+          strategy: 'local'
+        }, credentials);
+        await client.authenticate(payload);
+      }
+
+      // If successful, show the chat page
+      this.showViz();
+    } catch (error) {
+      // If we got an error, show the login page
+      this.showLogin(error);
+    }
+  }
+
+  async signup(credentials) {
+    try {
+      // First create the user
+      await client.service('users').create(credentials, {
+        headers: {
+          'X-Requested-With': 'FeathersJS'
+        }
+      });
+
+    } catch (error) {
+      this.showLogin(error);
+    }
+  }
+
+  setup() {
+    document.addEventListener('click', async ev => {
+      switch (ev.target.id) {
+        case 'signup':
+          {
+            // For signup, create a new user and then log them in
+            const user = this.getCredentials();
+
+            await this.signup(user);
+            // If successful log them in
+            await this.login(user);
+
+            break;
+          }
+        case 'login':
+          {
+            const user = this.getCredentials();
+
+            await this.login(user);
+
+            break;
+          }
+        case 'logout':
+          {
+            await client.logout();
+
+            select('#app').elt.innerHTML = views.loginHTML;
+
+            break;
+          }
+        case 'admin':
+          {
+            await this.showAdmin();
+            break;
+          }
+        case 'submitFeelings':
+          {
+            await this.submitFeelings();
+
+            break;
+          }
+        case 'vis':
+          {
+
+            await client.authenticate();
+            await this.showViz();
+            break;
+          }
+
+      }
+    });
+  }
+
+}
